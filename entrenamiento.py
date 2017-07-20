2fd074c7a121fea22313bb792bcf732662ffd65f
MVerdin <jmvl1303@gmail.com>
2017-07-20 16:52:54 -0500

    errores arreglados

--- entrenamiento.py
+++ entrenamiento.py
@@ -2,6 +2,7 @@
 import entrenamientoGUI as egui
 import wx
 import tensorflow.contrib.keras as keras
+import modelos
 contador=0
 
 
@@ -9,18 +10,19 @@ def OnButtonEntrenar(evnt):
 #    global contador
 #    contador += 1
 #    app.frame.textConsola.SetLabel("Hola {}-{}".format(contador,app.frame.intextRutaModelo.GetValue()))
-    
+
     ruta_modelo=app.frame.intextRutaModelo.GetValue()
     ruta_datos=app.frame.intextRutaDatos.GetValue()
     lr=app.frame.intextLR.GetValue()
     optimizador="adam" if app.frame.radioADAM.GetValue() else "cgd"
     tensorboard=app.frame.checkboxTensorboard.GetValue()
-    Entrenar(ruta_modelo,optimizador,lr,tensorboard,ruta_datos)
+    Entrenar(modelos.GenerarModelo(180,240,5),optimizador,lr,tensorboard,ruta_datos)
 
 def OnButtonCancelar(evnt):
 #    global contador
 #    contador -= 1
 #    app.frame.textConsola.SetLabel("Hola {}".format(contador))
+    pass
 
 def Entrenar(modelo, optimizador, lr, tb, carpetadatos):
     print("Modelo: {} |Optimizador: {} |LR: {} |TB: {} |Datos para ent: {}".format(modelo, optimizador, lr, tb, carpetadatos))
--- modelos.py
+++ modelos.py
@@ -1,28 +1,28 @@
 import tensorflow.contrib.keras as keras
 
 def GenerarModelo(anchoimg,altoimg,clases):
-    entradaImagen = keras.layers.Input(shape=(altoimg,anchoimg))
+    entradaImagen = keras.layers.Input(shape=(altoimg,anchoimg,1))
 
     conv1 = keras.layers.Conv2D(64, (3,3))(entradaImagen)
     pool1 = keras.layers.MaxPooling2D((2,2))(conv1)
-    norm1 = keras.layers.normalization.BatchNormalization()(pool1)
+    norm1 = keras.layers.BatchNormalization()(pool1)
 
     conv2 = keras.layers.Conv2D(64, (3,3))(norm1)
     pool2 = keras.layers.MaxPooling2D((2,2))(conv2)
-    norm2 = keras.layers.normalization.BatchNormalization()(pool2)
+    norm2 = keras.layers.BatchNormalization()(pool2)
 
     conv3 = keras.layers.Conv2D(64, (3,3))(norm2)
     pool3 = keras.layers.MaxPooling2D((2,2))(conv3)
-    norm3 = keras.layers.normalization.BatchNormalization()(pool3)
+    norm3 = keras.layers.BatchNormalization()(pool3)
 
-    flat = keras.layers.core.Flatten()(norm3)
+    flat = keras.layers.Flatten()(norm3)
 
-    dense1 = keras.layers.core.Dense(128, activation = "relu")(flat)
-    dropout1 = keras.ayers.core.Dropout(0.3)(dense1)
-    dense2 = keras.layers.core.Dense(128, activation = "relu")(dropout1)
+    dense1 = keras.layers.Dense(128, activation = "relu")(flat)
+    dropout1 = keras.layers.Dropout(0.3)(dense1)
+    dense2 = keras.layers.Dense(128, activation = "relu")(dropout1)
 
-    out = keras.layers.core.Dense(clases, activation = "softmax")(dense2)
+    out = keras.layers.Dense(clases, activation = "softmax")(dense2)
 
-    modelo = Model(inputs=entradaImagen, outputs = out)
+    modelo = keras.models.Model(inputs=entradaImagen, outputs = out)
 
     return modelo
