62e3488fe241e1f69263c21dbb6dc5af8dbc0396
MVerdin <jmvl1303@gmail.com>
2017-07-22 13:24:21 -0500

    carga de modelo

--- entrenamiento.py
+++ entrenamiento.py
@@ -3,29 +3,33 @@ import entrenamientoGUI as egui
 import wx
 import tensorflow.contrib.keras as keras
 import modelos
+import os
 contador=0
 
 
 def OnButtonEntrenar(evnt):
-#    global contador
-#    contador += 1
-#    app.frame.textConsola.SetLabel("Hola {}-{}".format(contador,app.frame.intextRutaModelo.GetValue()))
+    ruta_modelo = app.frame.intextRutaModelo.GetValue()
 
-    ruta_modelo=app.frame.intextRutaModelo.GetValue()
-    ruta_datos=app.frame.intextRutaDatos.GetValue()
-    lr=app.frame.intextLR.GetValue()
-    optimizador="adam" if app.frame.radioADAM.GetValue() else "cgd"
-    tensorboard=app.frame.checkboxTensorboard.GetValue()
+    if app.frame.checkboxContinuarEnt.GetValue():
+        if os.path.isfile(ruta_modelo):
+            try:
+                modelo = keras.models.load_model(ruta_modelo)
+            except ValueError:
+                print("Archivo invalido")
+
+    ruta_datos = app.frame.intextRutaDatos.GetValue()
+    lr = app.frame.intextLR.GetValue()
+    optimizador = "adam" if app.frame.radioADAM.GetValue() else "cgd"
+    tensorboard = app.frame.checkboxTensorboard.GetValue()
+
+
     Entrenar(modelos.GenerarModelo(180,240,5),optimizador,lr,tensorboard,ruta_datos)
 
 def OnButtonCancelar(evnt):
-#    global contador
-#    contador -= 1
-#    app.frame.textConsola.SetLabel("Hola {}".format(contador))
     pass
 
 def Entrenar(modelo, optimizador, lr, tb, carpetadatos):
-    print("Modelo: {} |Optimizador: {} |LR: {} |TB: {} |Datos para ent: {}".format(modelo, optimizador, lr, tb, carpetadatos))
+    print(Optimizador: {} | LR: {} | TB: {} | Datos para ent: {}".format(optimizador, lr, tb, carpetadatos))
 
 
 if __name__ == "__main__":
