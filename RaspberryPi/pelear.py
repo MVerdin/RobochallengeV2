00540ec2b2806123cba432889f9c6448a26d724f
MVerdin <jmvl1303@gmail.com>
2017-07-30 14:57:42 -0500

    Adaptada forma de imagen para entrada de modelo

--- RaspberryPi/pelear.py
+++ RaspberryPi/pelear.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python3
 import sys, os
 sys.path.insert(len(sys.path), os.path.abspath(os.path.join(os.getcwd(), os.pardir)))
 import configuracion
 
 import tensorflow.contrib.keras as keras
 import picamera
+import picamera.array
+import numpy as np
 
 resolucion_camara, escala_de_grises = configuracion.ObtenerConfigPelea()
 with picamera.PiCamera(sensor_mode=6,resolution=resolucion_camara) as camera:
     with picamera.array.PiRGBArray(camera) as output:
         camera.capture(output, 'rgb',True)
-        print(output.shape)
+        print(np.expand_dims(output.array,0).shape)
