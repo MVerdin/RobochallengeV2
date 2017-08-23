6caad9108e70ac65b2420199fec28ce00a6c6095
MVerdin <jmvl1303@gmail.com>
2017-08-22 23:43:25 -0500

    Agregada instalacion de tensorflow

--- RaspberryPi/preparacion_raspberry.sh
+++ RaspberryPi/preparacion_raspberry.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd ~
 
 sudo raspi-config
 sudo apt update
@@ -16,6 +17,11 @@ sudo pip3 install pybluez
 sudo pip3 install numpy
 sudo pip3 install RPi.GPIO
 sudo pip3 install picamera
+
+wget https://github.com/samjabrahams/tensorflow-on-raspberry-pi/releases/download/v1.1.0/tensorflow-1.1.0-cp34-cp34m-linux_armv7l.whl
+sudo pip3 install tensorflow-1.1.0-cp34-cp34m-linux_armv7l.whl
+sudo pip3 uninstall mock
+sudo pip3 install mock
 
 sudo apt install build-essential cmake pkg-config
 sudo apt install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
