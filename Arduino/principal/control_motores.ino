6e3816d875a78dd8a86129708f991f7ee77c6c25
MVerdin <jmvl1303@gmail.com>
2017-10-09 22:56:37 -0500

    agregado control de motores y freno de seguridad en arduino

--- Arduino/principal/control_motores.ino
+++ Arduino/principal/control_motores.ino
@@ -1,10 +1,124 @@
-//0 detenido, 1 avanzando, 2 retrocediendo, 3 girandoderecha, 4 girandoizquierda
-int estado = 0;
+
+
+
+
+unsigned long tiempo_inicio; 
+boolean cambio_en_espera = false;
+int estado_salidas = 0;
+int estado_en_espera = 0;
+int estado_anterior = 0;
+
+void analizar_estado(){
+    switch(estado){
+        case 0:
+            estado_salidas=estado;
+            break;
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            if(cambio_en_espera && estado != estado_en_espera){
+                if(estado == estado_anterior){
+                    estado_salidas = estado;
+                    cambio_en_espera = false;
+                    estado_en_espera= 0;
+                    estado_anterior=0;
+                }
+                else{
+                    estado_en_espera=estado;
+                }
+            }
+            else{
+                if(estado != 0 && estado != estado_salidas && estado_salidas!=0){
+                    cambio_en_espera=true;
+                    tiempo_inicio=millis();
+                    estado_en_espera = estado;
+                    estado_anterior = estado_salidas;
+                    estado_salidas=0;
+                }
+                else{
+                    estado_salidas=estado;
+                }
+            }
+            break;
+        default:
+            estado_salidas=0;
+            break;
+    }
+    if(cambio_en_espera){
+        if(millis()-tiempo_inicio > tiemposeguridad){
+            estado_salidas = estado_en_espera;
+            cambio_en_espera = false;
+        }
+        else{
+            estado_salidas=0;
+        }
+    }
+    cambiar_salidas();
+}
+
+void cambiar_salidas(){
+    switch(estado_salidas){
+        case 0:
+            detenerse();
+            break;
+        case 1:
+            avanzar();
+            break;
+        case 2:
+            retroceder();
+            break;
+        case 3:
+            girarderecha();
+            break;
+        case 4:
+            girarizquierda();
+            break;
+        default:
+            detenerse();
+            break;
+    }
+}
+
+void avanzar(){
+    digitalWrite(pinmotorA1, HIGH);
+    digitalWrite(pinmotorA2, LOW);
+    digitalWrite(pinmotorB1, HIGH);
+    digitalWrite(pinmotorB2, LOW);
+}
+
+void retroceder(){
+    digitalWrite(pinmotorA1, LOW);
+    digitalWrite(pinmotorA2, HIGH);
+    digitalWrite(pinmotorB1, LOW);
+    digitalWrite(pinmotorB2, HIGH);
+}
+
+void girarderecha(){
+    digitalWrite(pinmotorA1, HIGH);
+    digitalWrite(pinmotorA2, LOW);
+    digitalWrite(pinmotorB1, LOW);
+    digitalWrite(pinmotorB2, HIGH);
+}
+
+void girarizquierda(){
+    digitalWrite(pinmotorA1, LOW);
+    digitalWrite(pinmotorA2, HIGH);
+    digitalWrite(pinmotorB1, HIGH);
+    digitalWrite(pinmotorB2, LOW);
+}
+
+void detenerse(){
+    digitalWrite(pinmotorA1, LOW);
+    digitalWrite(pinmotorA2, LOW);
+    digitalWrite(pinmotorB1, LOW);
+    digitalWrite(pinmotorB2, LOW);
+}
 
 void procesar_comando_motores(String comando){
     comando.remove(0,1);
     if(comando=="estado"){
-        switch(estado){
+        switch(estado_salidas){
             case 0:
                 Serial.println("detenido");
                 break;
@@ -23,7 +137,6 @@ void procesar_comando_motores(String comando){
             default:
                 break;
         }
-
     }
     else if(comando=="avanzar"){
         estado=1;
--- Arduino/principal/principal.ino
+++ Arduino/principal/principal.ino
@@ -1,9 +1,25 @@
 
+//Configuracion
+const int tiemposeguridad = 500;
+
+//Pines de conexion
+const int pinmotorA1 = 22;
+const int pinmotorA2 = 24;
+const int pinmotorB1 = 26;
+const int pinmotorB2 = 28;
+
+
+//0 detenido, 1 avanzando, 2 retrocediendo, 3 girandoderecha, 4 girandoizquierda
+int estado = 0;
 
 String mensaje;
 
 void setup() {
   Serial.begin(38400);
+  pinMode(pinmotorA1, OUTPUT);
+  pinMode(pinmotorA2, OUTPUT);
+  pinMode(pinmotorB1, OUTPUT);
+  pinMode(pinmotorB2, OUTPUT);
 
 }
 
@@ -16,7 +32,5 @@ void loop() {
       procesar_comando_motores(mensaje);
     }
   }
-
-  
-
+  analizar_estado();
 }
